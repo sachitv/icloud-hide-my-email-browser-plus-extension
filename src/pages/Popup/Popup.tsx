0fc80dfe5379f0985e26e8660c2f2b6c0534636e
Sachit Vithaldas <1705581+sachitv@users.noreply.github.com>
2025-10-17 00:11:00 -0700

    chore: fix some more typos (#57)

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -683,7 +683,7 @@ const HmeManager = (props: {
           type="search"
           className="w-full rounded-2xl border border-slate-700 bg-slate-900 py-2 pl-10 pr-3 text-sm text-slate-100 placeholder-slate-500 focus:border-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-400/60"
           placeholder="Search"
-          aria-label="Search through your HideMyEmail+ aliases"
+          aria-label="Search through your Hide My Email+ aliases"
           onChange={(e) => {
             setSearchPrompt(e.target.value);
             setSelectedHmeIdx(0);
